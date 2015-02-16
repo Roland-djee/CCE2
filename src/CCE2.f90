0ad3c6a271bd19dffcc8478caf50dc4b687eeb6c
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-16 16:13:10 +0000

    Add doxygen doc.

--- src/CCE2.f90
+++ src/CCE2.f90
@@ -1,10 +1,29 @@
+!------------------------------------------------------------------------------
+! CCE2 main code
+!------------------------------------------------------------------------------
+!
+! MAIN: CCE2
+!
+!> @author
+!> Dr. Roland Guichard University College London
+!
+! DESCRIPTION: 
+!> This part of the CCE2 code is the main. 
+!
+! REVISION HISTORY:
+! 16-02-2015 - Initial Version
+! TODO_16_02_2015 - Complete main - TODO_main
+!------------------------------------------------------------------------------
+
 program CCE2
   use type
   use read
   use system_basis
+  use build_hamiltonian
   implicit none
   integer :: i,j
 
+!> Read the system basis from input
   call read_basis
 
   print*,basis(1)%spin_type
@@ -14,6 +33,7 @@ program CCE2
   print*,basis(1)%spin_mt
   print*,basis(2)%spin_mt
 
+!> Create the basis
   call create_basis
 
   do i=1,2
@@ -21,5 +41,8 @@ program CCE2
         print*,basis(i)%vector(j)
      end do
   end do
+
+!> Build the diagonal free-Hamiltonian
+  call build_diag
 
 end program CCE2
