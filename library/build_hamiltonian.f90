d51ab6d3207f457d05aac17f94d618dffa131dbd
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-16 17:47:19 +0000

    Add build_diag subroutine.

--- library/build_hamiltonian.f90
+++ library/build_hamiltonian.f90
@@ -13,10 +13,12 @@
 !
 ! REVISION HISTORY:
 ! 16-02-2015 - Initial Version
-! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
+! TODO_16_02_2015 - Finish build_diag - TODO_build_hamiltonian
 !------------------------------------------------------------------------------
 
 module build_hamiltonian
+  use type
+  use constant
   implicit none
 
 contains
@@ -28,8 +30,7 @@ contains
   ! DESCRIPTION: 
   !> Computes the diagonal part of the free Hamiltonian
   !> @brief
-  !> Flow method (rate of change of position) used by integrator.
-  !> Compute \f$ \frac{d\lambda}{dt} , \frac{d\phi}{dt},  \frac{dz}{dt} \f$
+  !> Computes the Zeeman terms of the Hamiltonian.
   !
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
@@ -41,8 +42,18 @@ contains
 
   subroutine build_diag
     implicit none
+    integer :: i,j,k
+    double precision :: factor,B0
 
-  
+    factor = 2.d0 * pi * B0
+    
+    k = 0
+    do i=1,basis(1)%spin_mt
+       do j=1,basis(2)%spin_mt
+          k = k + 1
+          H0_diag(k) = 1.d0
+       end do
+    end do
 
   end subroutine build_diag
 
