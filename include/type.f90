51d6c153ef9a87e34fa188b97d324084a5bb4418
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-16 16:08:00 +0000

    Add doxygen doc.

--- include/type.f90
+++ include/type.f90
@@ -1,3 +1,20 @@
+!------------------------------------------------------------------------------
+! CCE2 module
+!------------------------------------------------------------------------------
+!
+! MODULE: type
+!
+!> @author
+!> Dr. Roland Guichard University College London
+!
+! DESCRIPTION: 
+!> Defines all variable, array etc. types needed for CCE2
+!
+! REVISION HISTORY:
+! 16-02-2015 - Initial Version
+! TODO_16_02_2015 - Complete main - TODO_main
+!------------------------------------------------------------------------------
+
 module type
   implicit none
 
