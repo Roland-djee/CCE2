979b9b34c4361aba5a8dc5379449e7390230156b
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-16 17:53:27 +0000

    Add constant.f90 to tracking files.

---
+++ include/constant.f90
@@ -0,0 +1,25 @@
+!------------------------------------------------------------------------------
+! CCE2 module
+!------------------------------------------------------------------------------
+!
+! MODULE: constant
+!
+!> @author
+!> Dr. Roland Guichard University College London
+!
+! DESCRIPTION: 
+!> Defines all physical constants needed for CCE2
+!
+! REVISION HISTORY:
+! 16-02-2015 - Initial Version
+!------------------------------------------------------------------------------
+
+module constant
+  implicit none
+
+  double precision, parameter :: pi = 3.141592653589793d0
+  double precision, parameter :: gamma_e      = 1.7591d5
+  double precision, parameter :: gamma_n_29Si = 53.1903d0
+  
+
+end module constant
