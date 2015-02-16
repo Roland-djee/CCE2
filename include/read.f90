484f9d05c1e79aed81e177f374feec27fbe6c54f
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-16 16:07:20 +0000

    Add doxygen doc.

--- include/read.f90
+++ include/read.f90
@@ -1,3 +1,20 @@
+!------------------------------------------------------------------------------
+! CCE2 module
+!------------------------------------------------------------------------------
+!
+! MODULE: read
+!
+!> @author
+!> Dr. Roland Guichard University College London
+!
+! DESCRIPTION: 
+!> Reads all input files. 
+!
+! REVISION HISTORY:
+! 16-02-2015 - Initial Version
+! TODO_16_02_2015 - Complete main - TODO_main
+!------------------------------------------------------------------------------
+
 module read
   use type
   implicit none
@@ -6,29 +23,47 @@ module read
 
 contains
 
-subroutine read_basis
-  implicit none
-  integer :: basis_inp = 10
+  !---------------------------------------------------------------------------  
+  !> @author 
+  !> Dr. Roland Guichard University College London
+  !
+  ! DESCRIPTION: 
+  !> Reads input file basis.inp
+  !> @brief
+  !> Flow method (rate of change of position) used by integrator.
+  !> Compute \f$ \frac{d\lambda}{dt} , \frac{d\phi}{dt},  \frac{dz}{dt} \f$
+  !
+  ! REVISION HISTORY:
+  ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
+  !
+  !> @param[in]  basis.inp   
+  !> @param[out] --      
+  !> @return     basis
+  !---------------------------------------------------------------------------
 
-  open(unit = basis_inp, file = '../input/basis.inp')
+  subroutine read_basis
+    implicit none
+    integer :: basis_inp = 10
+    
+    open(unit = basis_inp, file = '../input/basis.inp')
 
-  read (basis_inp, fmt_str) basis(1)%spin_type
-  if (basis(1)%spin_type == 'CS') then
-  else if (trim(basis(1)%spin_type) == 'Bath') then
-  else
-     write(*,*)'Input Basis',trim(basis(1)%spin_type),' not recognized...'
-     stop
-  end if
-  read (basis_inp, fmt_dbl) basis(1)%spin_mag
-  read (basis_inp, fmt_str) basis(2)%spin_type
-  read (basis_inp, fmt_dbl) basis(2)%spin_mag
-
-  basis(1)%spin_mt = int(2.d0 * basis(1)%spin_mag + 1.d0)
-  basis(2)%spin_mt = int(2.d0 * basis(2)%spin_mag + 1.d0)
-  
-  allocate (basis(1)%vector(basis(1)%spin_mt))
-  allocate (basis(2)%vector(basis(2)%spin_mt))
-
-end subroutine read_basis
+    read (basis_inp, fmt_str) basis(1)%spin_type
+    if (basis(1)%spin_type == 'CS') then
+    else if (trim(basis(1)%spin_type) == 'Bath') then
+    else
+       write(*,*)'Input Basis',trim(basis(1)%spin_type),' not recognized...'
+       stop
+    end if
+    read (basis_inp, fmt_dbl) basis(1)%spin_mag
+    read (basis_inp, fmt_str) basis(2)%spin_type
+    read (basis_inp, fmt_dbl) basis(2)%spin_mag
+    
+    basis(1)%spin_mt = int(2.d0 * basis(1)%spin_mag + 1.d0)
+    basis(2)%spin_mt = int(2.d0 * basis(2)%spin_mag + 1.d0)
+    
+    allocate (basis(1)%vector(basis(1)%spin_mt))
+    allocate (basis(2)%vector(basis(2)%spin_mt))
+    
+  end subroutine read_basis
 
 end module read
