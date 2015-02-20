3a88a16e4680dd720e83be02d905efc64993e777
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-20 10:07:48 +0000

    Add types for the B field and the spin operators.

--- include/type.f90
+++ include/type.f90
@@ -14,6 +14,7 @@
 ! 16-02-2015 - Initial Version
 ! TODO_16_02_2015 - Deallocate basis%vector - TODO_type
 ! TODO_16_02_2015 - Deallocate H0_diag - TODO_type
+! TODO_19_02_2015 - Deallocate H_hf,Sz,Sp,Sm - TODO_type
 !------------------------------------------------------------------------------
 
 module type
@@ -22,10 +23,20 @@ module type
   integer :: basis_nb
 
   double precision, allocatable :: H0_diag(:)
+  double precision, allocatable :: H_hf(:)
+  double precision, allocatable :: Sz(:,:),Sp(:,:),Sm(:,:)
+
+  type vector
+     sequence
+     integer :: x,y,z
+     double precision :: ampli
+  end type vector
+
+  type (vector) :: B0
 
   type basis_def
      sequence
-     character (len=20) :: spin_type
+     character (len=20) :: spin_type, spin_sp
      integer :: spin_mt
      double precision   :: spin_mag
      double precision, allocatable :: vector(:)
