750831aa168965901a3804235551f421d5400ed0
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2015-02-20 10:06:20 +0000

    Simplify read to the basis necessary parameters.

--- include/read.f90
+++ include/read.f90
@@ -17,9 +17,13 @@
 
 module read
   use type
+  use constant
   implicit none
-  character (len=*), parameter :: fmt_str = "(t50, a)"
-  character (len=*), parameter :: fmt_dbl = "(t50, es)"
+  character (len=*), parameter :: fmt_str  = "(t50, a)"
+  character (len=*), parameter :: fmt_str2 = "(t50, 2a)"
+  character (len=*), parameter :: fmt_int  = "(t50, i)"
+  character (len=*), parameter :: fmt_int3 = "(t50, 3i)"
+  character (len=*), parameter :: fmt_dbl  = "(t50, es)"
 
 contains
 
@@ -30,7 +34,7 @@ contains
   ! DESCRIPTION: 
   !> Reads input file basis.inp
   !> @brief
-  !> Sets all variables for the vector basis of the spin system.
+  !> Sets all variables for the basis of the spin system.
   !
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
@@ -46,23 +50,16 @@ contains
     
     open(unit = basis_inp, file = '../input/basis.inp')
 
+    read (basis_inp, fmt_int3) B0%x,B0%y,B0%z
+    read (basis_inp, fmt_dbl) B0%ampli
     read (basis_inp, fmt_str) basis(1)%spin_type
-    if (basis(1)%spin_type == 'CS') then
-    else if (trim(basis(1)%spin_type) == 'Bath') then
-    else
+    read (basis_inp, fmt_str) basis(1)%spin_sp
+    if (basis(1)%spin_type .ne. 'CS' .and. basis(1)%spin_type .ne. 'Bath') then
        write(*,*)'Input Basis',trim(basis(1)%spin_type),' not recognized...'
        stop
     end if
-    read (basis_inp, fmt_dbl) basis(1)%spin_mag
     read (basis_inp, fmt_str) basis(2)%spin_type
-    read (basis_inp, fmt_dbl) basis(2)%spin_mag
-    
-    basis(1)%spin_mt = int(2.d0 * basis(1)%spin_mag + 1.d0)
-    basis(2)%spin_mt = int(2.d0 * basis(2)%spin_mag + 1.d0)
-    
-    allocate (basis(1)%vector(basis(1)%spin_mt))
-    allocate (basis(2)%vector(basis(2)%spin_mt))
-    allocate (H0_diag(basis(1)%spin_mt * basis(2)%spin_mt))
+    read (basis_inp, fmt_str) basis(2)%spin_sp
     
   end subroutine read_basis
 
